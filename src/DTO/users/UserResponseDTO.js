348330587dc187cf0c358bbb47c8748ef12c9e6c
smoreno2508 <emorenocoder@gmail.com>
2024-03-28 22:19:18 -0300

    se agrega Documentación de API y Testing Avanzado

---
+++ src/DTO/users/UserResponseDTO.js
@@ -0,0 +1,18 @@
+export default class UserResponseDTO {
+    constructor(user){
+        this.id = user._id;
+        this.firstName = user.firstName;
+        this.lastName = user.lastName;
+        this.email = user.email;
+        this.role = user.role;
+        this.cartId = user.cartId;
+        this.isGithub = user.isGithub;
+        this.last_connection = user.last_connection;
+        this.documents = user.documents;
+
+    }
+
+    static fromModel(user){
+        return new UserResponseDTO(user);
+    }
+}
